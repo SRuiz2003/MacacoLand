aff7578672f1a0f5feaccb5c60afefd6ec05ed6b
david-ale <david_ale.sanchez@uao.edu.co>
2023-04-24 00:01:48 -0500

    Update ProfileInfo.jsx

--- Frontend/src/Components/ProfileInfo.jsx
+++ Frontend/src/Components/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import '../Pages/Index/index.css'
 export const ProfileInfo = ({username,numberOfPost,followers,followedPets,city,userDescription}) =>{
     return (
-        <div className='margin-left-25'>
+        <div className='margin-left-25  '>
             <div className='margin-top-info margin-left margin-bottom-25 margin-right'>
                 <div className='margin-bottom-15'><span><strong>Nombre de Usuario:</strong>{username}</span></div>
                 <div className='margin-bottom-15'><span className='margin-right'><strong>Publicaciones: </strong>{numberOfPost}</span> <span className='margin-right'> <strong>Seguidores: </strong>{followers}</span> <span><strong>Mascotas seguidas:</strong>{followedPets}</span></div>
